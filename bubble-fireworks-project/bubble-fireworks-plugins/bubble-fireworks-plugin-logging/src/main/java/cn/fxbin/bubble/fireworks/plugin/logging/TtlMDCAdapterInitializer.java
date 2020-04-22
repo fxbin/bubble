2eda1285f8591449ea97441ee0a921b39f419e34
fxbin <fxbin123@gmail.com>
2020-04-22 19:48:12 +0800

    日志引入ttl

---
+++ bubble-fireworks-project/bubble-fireworks-plugins/bubble-fireworks-plugin-logging/src/main/java/cn/fxbin/bubble/fireworks/plugin/logging/TtlMDCAdapterInitializer.java
@@ -0,0 +1,21 @@
+package cn.fxbin.bubble.fireworks.plugin.logging;
+
+import org.slf4j.TtlMDCAdapter;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * TtlMDCAdapterInitializer
+ * 初始化TtlMDCAdapter实例，并替换MDC 中的adapter 对象
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2020/4/22 16:13
+ */
+public class TtlMDCAdapterInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        // 加载TtlMDCAdapter 实例
+        TtlMDCAdapter.getMdcAdapter();
+    }
+}
