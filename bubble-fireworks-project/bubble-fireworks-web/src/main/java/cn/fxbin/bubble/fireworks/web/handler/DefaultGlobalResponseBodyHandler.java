50f3a877a22b555189d9c913776c51eab38d43de
fxbin <fxbin123@gmail.com>
2020-08-18 02:07:21 +0800

    default response ignore swagger resources

--- bubble-fireworks-project/bubble-fireworks-web/src/main/java/cn/fxbin/bubble/fireworks/web/handler/DefaultGlobalResponseBodyHandler.java
+++ bubble-fireworks-project/bubble-fireworks-web/src/main/java/cn/fxbin/bubble/fireworks/web/handler/DefaultGlobalResponseBodyHandler.java
@@ -1,12 +1,15 @@
 package cn.fxbin.bubble.fireworks.web.handler;
 
 import cn.fxbin.bubble.fireworks.core.model.Result;
+import com.google.common.collect.Lists;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.List;
 
 /**
  * DefaultGlobalResponseBodyHandler
@@ -15,8 +18,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * @version v1.0
  * @since 2020/7/28 10:22
  */
+@SuppressWarnings("ALL")
 @RestControllerAdvice
 public class DefaultGlobalResponseBodyHandler implements ResponseBodyAdvice {
+
+    private final static List<String> SWAGGER_RESOURCES = Lists.newArrayList("/swagger-resources", "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security");
+
     @Override
     public boolean supports(MethodParameter methodParameter, Class aClass) {
         return true;
@@ -25,6 +32,12 @@ public class DefaultGlobalResponseBodyHandler implements ResponseBodyAdvice {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType,
                                   ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+        String path = serverHttpRequest.getURI().getPath();
+
+        if (SWAGGER_RESOURCES.contains(path)) {
+            return body;
+        }
 
         // 如果已经是 Result 类型，则直接返回
         if (body instanceof Result) {
