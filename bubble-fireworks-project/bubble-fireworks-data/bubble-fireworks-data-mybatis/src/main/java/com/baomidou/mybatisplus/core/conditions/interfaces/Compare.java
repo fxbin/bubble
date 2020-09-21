662dc9a4a8c165dce48c41108518f02f4964c230
fxbin <fxbin123@gmail.com>
2020-09-21 19:02:59 +0800

    remove un-used import package

--- bubble-fireworks-project/bubble-fireworks-data/bubble-fireworks-data-mybatis/src/main/java/com/baomidou/mybatisplus/core/conditions/interfaces/Compare.java
+++ bubble-fireworks-project/bubble-fireworks-data/bubble-fireworks-data-mybatis/src/main/java/com/baomidou/mybatisplus/core/conditions/interfaces/Compare.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 
 import java.io.Serializable;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.BiPredicate;
 
 /**
