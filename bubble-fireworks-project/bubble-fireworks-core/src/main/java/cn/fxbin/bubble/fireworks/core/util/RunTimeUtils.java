5dcda7702ec77efc2ea4dcbaf0808c14f3130c92
fxbin <fxbin123@gmail.com>
2020-10-28 18:35:45 +0800

    add xxl-job auto configuration && customize job handler

--- bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/RunTimeUtils.java
+++ bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/RunTimeUtils.java
@@ -1,11 +1,17 @@
 package cn.fxbin.bubble.fireworks.core.util;
 
 import cn.fxbin.bubble.fireworks.core.constant.StringPool;
+import cn.hutool.core.util.RuntimeUtil;
+import lombok.Cleanup;
 import lombok.experimental.UtilityClass;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * RunTimeUtils
@@ -15,7 +21,7 @@ import java.util.List;
  * @since 2020/8/5 15:44
  */
 @UtilityClass
-public class RunTimeUtils {
+public class RunTimeUtils extends RuntimeUtil {
 
     /**
      * 进程pid
@@ -33,7 +39,7 @@ public class RunTimeUtils {
      *
      * @return success: 进程ID, failure: -1
      */
-    public static int getPid() {
+    public int getPid() {
         if(pid > 0 ) {
             return pid;
         }
@@ -53,7 +59,7 @@ public class RunTimeUtils {
      *
      * @return {@code Duration}
      */
-    public static Duration getUpTime() {
+    public Duration getUpTime() {
         long uptime = ManagementFactory.getRuntimeMXBean().getUptime();
         return Duration.ofMillis(uptime);
     }
@@ -64,7 +70,7 @@ public class RunTimeUtils {
      *
      * @return jvm 参数
      */
-    public static String getJvmArguments() {
+    public String getJvmArguments() {
         List<String> inputArguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
         return StringUtils.join(inputArguments, StringPool.SPACE);
     }
@@ -75,8 +81,113 @@ public class RunTimeUtils {
      *
      * @return cpu core number
      */
-    public static int getCpuNum() {
+    public int getCpuNum() {
         return CPU_NUM;
+    }
+
+    /**
+     * parseCmds
+     *
+     * @author fxbin
+     * @since 2020/10/28 12:00
+     * @param command (cmd|shell)命令
+     * @return java.lang.String[]
+     */
+    public String[] parseCmds(String command) {
+        return StringUtils.splitTrim(command, StringPool.SPACE);
+    }
+
+    /**
+     * getOptionValue
+     *
+     * @author fxbin
+     * @since 2020/10/28 13:43
+     * @param option command option
+     * @param command (cmd|shell)命令
+     * @return java.lang.String
+     */
+    public String getOptionValue(String option, String command) {
+        String[] args = parseCmds(command);
+        String optionValue = null;
+        for (int i = 0; i < args.length - 1; i++) {
+            if (Objects.equals(args[i], option)) {
+                optionValue = args[i + 1];
+                break;
+            }
+        }
+        return optionValue;
+    }
+
+    /**
+     * exec
+     *
+     * @author fxbin
+     * @since 2020/10/28 13:48
+     * @param timeout 超时时间(秒)
+     * @param cmds 命令列表，每个元素代表一条命令
+     * @return java.lang.String
+     */
+    public String exec(int timeout, String... cmds) {
+
+        int exitValue = -1;
+        Process process = null;
+        StringBuilder sbStd = new StringBuilder();
+        StringBuilder sbErr = new StringBuilder();
+
+        long start = SystemClock.INSTANCE.currentTimeMillis()/1000;
+        try {
+            process = exec(cmds);
+            @Cleanup BufferedReader brStd = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            @Cleanup BufferedReader brErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String line;
+
+            while (true) {
+                if (brStd.ready()) {
+                    line = brStd.readLine();
+                    sbStd.append(line).append("\n");
+                    continue;
+                }
+                if (brErr.ready()) {
+                    line = brErr.readLine();
+                    sbErr.append(line).append("\n");
+                    continue;
+                }
+
+                if (ObjectUtils.isNotEmpty(process)) {
+                    try {
+                        exitValue = process.exitValue();
+                        break;
+                    } catch (IllegalThreadStateException ignored) {
+                    }
+                }
+
+                if (SystemClock.INSTANCE.currentTimeMillis() / 1000 - start > timeout) {
+                    sbErr.append("\n命令执行超时退出.");
+                    break;
+                }
+
+                try {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            destroy(process);
+        }
+
+        if (sbErr.length() > 0) {
+            return sbErr.toString();
+        }
+        if (sbStd.length() > 0) {
+            return sbStd.toString();
+        }
+        if (exitValue == 0) {
+            return "success";
+        } else {
+            return "curl exit value(" + exitValue + ") is failed";
+        }
     }
 
 }
---
+++ bubble-fireworks-project/bubble-fireworks-plugins/bubble-fireworks-plugin-xxl-job/src/main/java/cn/fxbin/bubble/plugin/xxl/job/service/jobhandler/CustomizeXxlJob.java
@@ -0,0 +1,70 @@
+package cn.fxbin.bubble.plugin.xxl.job.service.jobhandler;
+
+import cn.fxbin.bubble.fireworks.core.util.JsonUtils;
+import cn.fxbin.bubble.fireworks.core.util.RunTimeUtils;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * CustomizeXxlJob
+ *
+ * 开发步骤：
+ * 1、在Spring Bean实例中，开发Job方法，方式格式要求为 "public ReturnT<String> execute(String param)"
+ * 2、为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
+ * 3、执行日志：需要通过 "XxlJobLogger.log" 打印执行日志；
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2020/10/28 11:43
+ */
+@Slf4j
+@Component
+public class CustomizeXxlJob {
+
+    private static final String INDEX_OF = "?";
+
+    @XxlJob("curlJobHandler")
+    public ReturnT<String> curlJobHandler(String param) {
+        log.info("Curl Job Start,param is:{}", param);
+        XxlJobLogger.log("Curl Job Start,param {}", param);
+        if (StringUtils.isEmpty(param)) {
+            XxlJobLogger.log("参数不能为空，请设置");
+            return new ReturnT<>(IJobHandler.FAIL.getCode(), "参数不能为空，请设置");
+        }
+
+        // 处理换行符
+        param = param.replace("\n","");
+
+        long timestamp = System.currentTimeMillis();
+        if(!param.contains(INDEX_OF)){
+            param += "?timestamp="+timestamp;
+        }else {
+            param += "&timestamp="+timestamp;
+        }
+
+        String command = "curl ";
+        String m = RunTimeUtils.getOptionValue("-m", param);
+        if (StringUtils.isEmpty(m)) {
+            command += "-m 1200 ";
+        }
+
+        String connectTimeout = RunTimeUtils.getOptionValue("--connect-timeout", param);
+        if (StringUtils.isEmpty(connectTimeout)) {
+            command += "--connect-timeout 1200 ";
+        }
+
+        command += param;
+
+        String result = RunTimeUtils.exec(86400, command);
+        XxlJobLogger.log("Curl Job End,param {},command {} result {}",param, command, JsonUtils.toJson(result));
+        log.info("Curl Job End, Param {}, Command {} Result {}", param, command, JsonUtils.toJson(result));
+
+        return new ReturnT<>(result);
+    }
+
+}
