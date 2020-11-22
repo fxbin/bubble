6f806701eab0639208854aa87c5df9e48a7f7f06
fxbin <fxbin123@gmail.com>
2020-11-22 19:43:46 +0800

    add OptimisticLockerInnerInterceptor & ISqlInjector bean

--- bubble-fireworks-project/bubble-fireworks-autoconfigure/src/main/java/cn/fxbin/bubble/fireworks/autoconfigure/data/mybatis/MybatisPlusAutoConfiguration.java
+++ bubble-fireworks-project/bubble-fireworks-autoconfigure/src/main/java/cn/fxbin/bubble/fireworks/autoconfigure/data/mybatis/MybatisPlusAutoConfiguration.java
@@ -3,9 +3,11 @@ package cn.fxbin.bubble.fireworks.autoconfigure.data.mybatis;
 import cn.fxbin.bubble.fireworks.data.mybatis.customizer.CustomizeIdentifierGenerator;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -34,20 +36,19 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class MybatisPlusAutoConfiguration {
 
-    /**
-     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false
-     * 避免缓存出现问题(该属性会在旧插件移除后一同移除)
-     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 
     @Bean
-    public ConfigurationCustomizer configurationCustomizer() {
-        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    public ISqlInjector sqlInjector() {
+        return new DefaultSqlInjector();
     }
 
     @Bean
