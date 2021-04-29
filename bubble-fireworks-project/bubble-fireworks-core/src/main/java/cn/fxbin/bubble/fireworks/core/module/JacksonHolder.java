19a652398f9b12c574b10acd26c2099a2c988e0a
fxbin <fxbin123@gmail.com>
2021-04-29 16:42:23 +0800

    feature(code): JacksonHolder 增加默认序列化配置

--- bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/module/JacksonHolder.java
+++ bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/module/JacksonHolder.java
@@ -1,6 +1,14 @@
 package cn.fxbin.bubble.fireworks.core.module;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.springframework.cache.support.NullValue;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 
 /**
  * JacksonHolder
@@ -11,6 +19,44 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public interface JacksonHolder {
 
-    ObjectMapper INSTANCE = new ObjectMapper().registerModule(new JavaTimeModule());
+    ObjectMapper INSTANCE = new ObjectMapper()
+            .registerModule(new JavaTimeModule().addSerializer(new NullValueSerializer((String) null)));
+
+    /**
+     * {@link StdSerializer} adding class information required by default typing. This allows de-/serialization of
+     * {@link NullValue}.
+     *
+     * @author Christoph Strobl
+     * @since 1.8
+     */
+    public class NullValueSerializer extends StdSerializer<NullValue> {
+
+        private static final long serialVersionUID = 1999052150548658808L;
+        private final String classIdentifier;
+
+        /**
+         * @param classIdentifier can be {@literal null} and will be defaulted to {@code @class}.
+         */
+        NullValueSerializer(@Nullable String classIdentifier) {
+
+            super(NullValue.class);
+            this.classIdentifier = StringUtils.hasText(classIdentifier) ? classIdentifier : "@class";
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see com.fasterxml.jackson.databind.ser.std.StdSerializer#serialize(java.lang.Object, com.fasterxml.jackson.core.JsonGenerator, com.fasterxml.jackson.databind.SerializerProvider)
+         */
+        @Override
+        public void serialize(NullValue value, JsonGenerator jgen, SerializerProvider provider)
+                throws IOException {
+
+            jgen.writeStartObject();
+            jgen.writeStringField(classIdentifier, NullValue.class.getName());
+            jgen.writeEndObject();
+        }
+    }
 
 }
+
+
