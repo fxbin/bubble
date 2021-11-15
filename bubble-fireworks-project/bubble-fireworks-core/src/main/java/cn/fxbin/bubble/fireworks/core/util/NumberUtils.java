da5af2ae28f03de93347ab8316875923f9b0ee2b
fxbin <fxbin123@gmail.com>
2021-11-15 18:01:06 +0800

    :art: 移除重复代码、未使用的包

--- bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/NumberUtils.java
+++ bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/NumberUtils.java
@@ -435,23 +435,5 @@ public class NumberUtils extends NumberUtil {
                 roundingMode
         );
     }
-
-
-    /**
-     * All possible chars for representing a number as a String
-     */
-    final byte[] DIGITS = {
-            '0' , '1' , '2' , '3' , '4' , '5' ,
-            '6' , '7' , '8' , '9' , 'a' , 'b' ,
-            'c' , 'd' , 'e' , 'f' , 'g' , 'h' ,
-            'i' , 'j' , 'k' , 'l' , 'm' , 'n' ,
-            'o' , 'p' , 'q' , 'r' , 's' , 't' ,
-            'u' , 'v' , 'w' , 'x' , 'y' , 'z' ,
-            'A' , 'B' , 'C' , 'D' , 'E' , 'F' ,
-            'G' , 'H' , 'I' , 'J' , 'K' , 'L' ,
-            'M' , 'N' , 'O' , 'P' , 'Q' , 'R' ,
-            'S' , 'T' , 'U' , 'V' , 'W' , 'X' ,
-            'Y' , 'Z'
-    };
     
 }
