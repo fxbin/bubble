8ca8ccbadf6f1177064a819c3e4181bb61d1e318
fxbin <fxbin123@gmail.com>
2021-12-02 14:12:41 +0800

    :recycle: 优化全局异常捕获

--- bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-web/src/main/java/cn/fxbin/bubble/fireworks/web/handler/DefaultGlobalExceptionHandler.java
+++ bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-web/src/main/java/cn/fxbin/bubble/fireworks/web/handler/DefaultGlobalExceptionHandler.java
@@ -3,19 +3,26 @@ package cn.fxbin.bubble.fireworks.web.handler;
 import cn.fxbin.bubble.fireworks.core.exception.ServiceException;
 import cn.fxbin.bubble.fireworks.core.model.Result;
 import cn.fxbin.bubble.fireworks.core.model.ResultCode;
-import cn.fxbin.bubble.fireworks.core.util.ObjectUtils;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static cn.fxbin.bubble.fireworks.core.model.ResultCode.REQUEST_PARAM_VALIDATION_ERROR;
 
@@ -30,46 +37,81 @@ import static cn.fxbin.bubble.fireworks.core.model.ResultCode.REQUEST_PARAM_VALI
 @RestControllerAdvice
 public class DefaultGlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public Result<String> exceptionHandler(Exception exception) {
         log.warn("[Exception]", exception);
         return Result.failure(exception.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = ServiceException.class)
     public Result<String> exceptionHandler(ServiceException ex) {
         log.warn("[ServiceException]", ex);
         return Result.failure((ex.getErrcode() == 0 ? ResultCode.FAILURE.getCode() : ex.getErrcode()), ex.getMessage());
     }
 
+    /**
+     * 处理 SpringMVC 请求参数缺失
+     *
+     * 例如说，接口上设置了 @RequestParam("xx") 参数，结果并未传递 xx 参数
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public Result<?> missingServletRequestParameterExceptionHandler(MissingServletRequestParameterException exception) {
+        log.warn("MissingServletRequestParameterExceptionHandler", exception);
+        return Result.failure(ResultCode.BAD_REQUEST, String.format("请求参数缺失:%s", exception.getParameterName()));
+    }
+
+    /**
+     * 处理 SpringMVC 请求参数类型错误
+     *
+     * 例如说，接口上设置了 @RequestParam("xx") 参数为 Integer，结果传递 xx 参数类型为 String
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public Result<?> methodArgumentTypeMismatchExceptionHandler(MethodArgumentTypeMismatchException exception) {
+        log.warn("MethodArgumentTypeMismatchExceptionHandler", exception);
+        return Result.failure(ResultCode.BAD_REQUEST, String.format("请求参数类型错误:%s", exception.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public Result<?> constraintViolationExceptionHandler(ConstraintViolationException exception) {
+        log.warn("ConstraintViolationExceptionHandler", exception);
+        ConstraintViolation<?> constraintViolation = exception.getConstraintViolations().iterator().next();
+        return Result.failure(ResultCode.BAD_REQUEST, String.format("请求参数不正确:%s", constraintViolation.getMessage()));
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public Result<String> bodyValidExceptionHandler(MethodArgumentNotValidException exception) {
+    public Result<?> bodyValidExceptionHandler(MethodArgumentNotValidException exception) {
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        // 取出所有的校验不通过的描述
+        List<String> fieldErrorMessages = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
         log.warn("MethodArgumentNotValidException: {}", exception.getMessage());
-        return Result.failure(fieldErrors.get(0).getDefaultMessage());
+        return Result.failure(ResultCode.BAD_REQUEST, fieldErrorMessages.toString());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({BindException.class})
-    public Result<String> bindExceptionHandler(BindException exception) {
-        @SuppressWarnings("ConstantConditions") String defaultMessage = exception.getGlobalError().getDefaultMessage();
+    public Result<?> bindExceptionHandler(BindException exception) {
         log.warn("BindException: {}", exception.getMessage());
-        return Result.failure(ObjectUtils.isEmpty(defaultMessage) ? REQUEST_PARAM_VALIDATION_ERROR.getMsg() : defaultMessage);
+        FieldError fieldError = exception.getFieldError();
+        // 断言，避免告警
+        assert fieldError != null;
+        return Result.failure(ResultCode.BAD_REQUEST,
+                ObjectUtils.isEmpty(fieldError.getDefaultMessage()) ? ResultCode.BAD_REQUEST.getMsg(): fieldError.getDefaultMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({JsonParseException.class})
-    public Result<String> exceptionHandler(JsonParseException exception) {
+    public Result<?> exceptionHandler(JsonParseException exception) {
         log.warn("JsonParseException: {}", exception.getMessage());
         return Result.failure(exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({JsonMappingException.class})
-    public Result<String> exceptionHandler(JsonMappingException exception) {
+    public Result<?> exceptionHandler(JsonMappingException exception) {
         log.warn("JsonMappingException: {}", exception.getMessage());
         return Result.failure(exception.getMessage());
     }
