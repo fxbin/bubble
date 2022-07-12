301cfd3bbe3add253ebbf8717e2c60aefa5c0bfa
fxbin <fxbin123@gmail.com>
2022-07-12 16:40:09 +0800

    :bug: mybatis-plus 排序问题修复

--- bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-data-mybatis/src/main/java/com/baomidou/mybatisplus/core/conditions/interfaces/Func.java
+++ bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-data-mybatis/src/main/java/com/baomidou/mybatisplus/core/conditions/interfaces/Func.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * Copyright (c) 2011-2022, baomidou (jobob@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.baomidou.mybatisplus.core.conditions.interfaces;
 
@@ -19,12 +19,9 @@ import com.baomidou.mybatisplus.core.toolkit.ArrayUtils;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Optional;
+import java.util.List;
 import java.util.function.Consumer;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * 查询条件封装
@@ -76,7 +73,6 @@ public interface Func<Children, R> extends Serializable {
         return in(true, column, coll);
     }
 
-
     /**
      * inIfPresent
      *
@@ -97,6 +93,7 @@ public interface Func<Children, R> extends Serializable {
      * 字段 IN (value.get(0), value.get(1), ...)
      * <p>例: in("id", Arrays.asList(1, 2, 3, 4, 5))</p>
      *
+     * <li> 注意！集合为空若存在逻辑错误，请在 condition 条件中判断 </li>
      * <li> 如果集合为 empty 则不会进行 sql 拼接 </li>
      *
      * @param condition 执行条件
@@ -113,17 +110,11 @@ public interface Func<Children, R> extends Serializable {
         return in(true, column, values);
     }
 
-    default Children inIfPresent(R column, Object... values) {
-        if (ArrayUtils.isNotEmpty(values)) {
-            return in(true, column, values);
-        }
-        return (Children) this;
-    }
-
     /**
      * 字段 IN (v0, v1, ...)
      * <p>例: in("id", 1, 2, 3, 4, 5)</p>
      *
+     * <li> 注意！数组为空若存在逻辑错误，请在 condition 条件中判断 </li>
      * <li> 如果动态数组为 empty 则不会进行 sql 拼接 </li>
      *
      * @param condition 执行条件
@@ -131,10 +122,7 @@ public interface Func<Children, R> extends Serializable {
      * @param values    数据数组
      * @return children
      */
-    default Children in(boolean condition, R column, Object... values) {
-        return in(condition, column, Arrays.stream(Optional.ofNullable(values).orElseGet(() -> new Object[]{}))
-            .collect(toList()));
-    }
+    Children in(boolean condition, R column, Object... values);
 
     /**
      * ignore
@@ -170,6 +158,12 @@ public interface Func<Children, R> extends Serializable {
      */
     Children notIn(boolean condition, R column, Collection<?> coll);
 
+    /**
+     * ignore
+     */
+    default Children notIn(R column, Object... value) {
+        return notIn(true, column, value);
+    }
 
     /**
      * notInIfPresent
@@ -188,13 +182,6 @@ public interface Func<Children, R> extends Serializable {
     }
 
     /**
-     * ignore
-     */
-    default Children notIn(R column, Object... value) {
-        return notIn(true, column, value);
-    }
-
-    /**
      * 字段 NOT IN (v0, v1, ...)
      * <p>例: notIn("id", 1, 2, 3, 4, 5)</p>
      *
@@ -203,10 +190,7 @@ public interface Func<Children, R> extends Serializable {
      * @param values    数据数组
      * @return children
      */
-    default Children notIn(boolean condition, R column, Object... values) {
-        return notIn(condition, column, Arrays.stream(Optional.ofNullable(values).orElseGet(() -> new Object[]{}))
-            .collect(toList()));
-    }
+    Children notIn(boolean condition, R column, Object... values);
 
     /**
      * ignore
@@ -229,6 +213,82 @@ public interface Func<Children, R> extends Serializable {
     Children inSql(boolean condition, R column, String inValue);
 
     /**
+     * 字段 &gt; ( sql语句 )
+     * <p>例1: gtSql("id", "1, 2, 3, 4, 5, 6")</p>
+     * <p>例1: gtSql("id", "select id from table where name = 'JunJun'")</p>
+     *
+     * @param condition
+     * @param column
+     * @param inValue
+     * @return
+     */
+    Children gtSql(boolean condition, R column, String inValue);
+
+    /**
+     * ignore
+     */
+    default Children gtSql(R column, String inValue) {
+        return gtSql(true, column, inValue);
+    }
+
+    /**
+     * 字段 >= ( sql语句 )
+     * <p>例1: geSql("id", "1, 2, 3, 4, 5, 6")</p>
+     * <p>例1: geSql("id", "select id from table where name = 'JunJun'")</p>
+     *
+     * @param condition
+     * @param column
+     * @param inValue
+     * @return
+     */
+    Children geSql(boolean condition, R column, String inValue);
+
+    /**
+     * ignore
+     */
+    default Children geSql(R column, String inValue) {
+        return geSql(true, column, inValue);
+    }
+
+    /**
+     * 字段 &lt; ( sql语句 )
+     * <p>例1: ltSql("id", "1, 2, 3, 4, 5, 6")</p>
+     * <p>例1: ltSql("id", "select id from table where name = 'JunJun'")</p>
+     *
+     * @param condition
+     * @param column
+     * @param inValue
+     * @return
+     */
+    Children ltSql(boolean condition, R column, String inValue);
+
+    /**
+     * ignore
+     */
+    default Children ltSql(R column, String inValue) {
+        return ltSql(true, column, inValue);
+    }
+
+    /**
+     * 字段 <= ( sql语句 )
+     * <p>例1: leSql("id", "1, 2, 3, 4, 5, 6")</p>
+     * <p>例1: leSql("id", "select id from table where name = 'JunJun'")</p>
+     *
+     * @param condition
+     * @param column
+     * @param inValue
+     * @return
+     */
+    Children leSql(boolean condition, R column, String inValue);
+
+    /**
+     * ignore
+     */
+    default Children leSql(R column, String inValue) {
+        return leSql(true, column, inValue);
+    }
+
+    /**
      * ignore
      */
     default Children notInSql(R column, String inValue) {
@@ -249,91 +309,154 @@ public interface Func<Children, R> extends Serializable {
     Children notInSql(boolean condition, R column, String inValue);
 
     /**
-     * ignore
+     * 分组：GROUP BY 字段, ...
+     * <p>例: groupBy("id")</p>
+     *
+     * @param condition 执行条件
+     * @param column    单个字段
+     * @return children
      */
+    Children groupBy(boolean condition, R column);
+
     default Children groupBy(R column) {
         return groupBy(true, column);
     }
 
     /**
-     * ignore
-     */
-    default Children groupBy(R... columns) {
-        return groupBy(true, columns);
-    }
-
-    /**
      * 分组：GROUP BY 字段, ...
-     * <p>例: groupBy("id", "name")</p>
+     * <p>例: groupBy(Arrays.asList("id", "name"))</p>
      *
      * @param condition 执行条件
      * @param columns   字段数组
      * @return children
      */
-    Children groupBy(boolean condition, R... columns);
+    Children groupBy(boolean condition, List<R> columns);
+
+    default Children groupBy(List<R> columns) {
+        return groupBy(true, columns);
+    }
+
+    default Children groupBy(R column, R... columns) {
+        return groupBy(true, column, columns);
+    }
 
     /**
-     * ignore
+     * 分组：GROUP BY 字段, ...
      */
+    Children groupBy(boolean condition, R column, R... columns);
+
+    /**
+     * 排序：ORDER BY 字段, ... ASC
+     * <p>例: orderByAsc(true, "id")</p>
+     *
+     * @param condition 执行条件
+     * @param column    单个字段
+     * @return children
+     */
+    default Children orderByAsc(boolean condition, R column) {
+        return orderBy(condition, true, column);
+    }
+
     default Children orderByAsc(R column) {
         return orderByAsc(true, column);
     }
 
     /**
-     * ignore
-     */
-    default Children orderByAsc(R... columns) {
-        return orderByAsc(true, columns);
-    }
-
-    /**
      * 排序：ORDER BY 字段, ... ASC
-     * <p>例: orderByAsc("id", "name")</p>
+     * <p>例: orderByAsc(true, Arrays.asList("id", "name"))</p>
      *
      * @param condition 执行条件
      * @param columns   字段数组
      * @return children
      */
-    default Children orderByAsc(boolean condition, R... columns) {
+    default Children orderByAsc(boolean condition, List<R> columns) {
         return orderBy(condition, true, columns);
     }
 
+    default Children orderByAsc(List<R> columns) {
+        return orderByAsc(true, columns);
+    }
+
+    default Children orderByAsc(R column, R... columns) {
+        return orderByAsc(true, column, columns);
+    }
+
     /**
-     * ignore
+     * 排序：ORDER BY 字段, ... ASC
      */
+    default Children orderByAsc(boolean condition, R column, R... columns) {
+        return orderBy(condition, true, column, columns);
+    }
+
+    /**
+     * 排序：ORDER BY 字段, ... DESC
+     * <p>例: orderByDesc(true, "id")</p>
+     *
+     * @param condition 执行条件
+     * @param column    字段
+     * @return children
+     */
+    default Children orderByDesc(boolean condition, R column) {
+        return orderBy(condition, false, column);
+    }
+
     default Children orderByDesc(R column) {
         return orderByDesc(true, column);
     }
 
     /**
-     * ignore
+     * 排序：ORDER BY 字段, ... DESC
+     * <p>例: orderByDesc(true, Arrays.asList("id", "name"))</p>
+     *
+     * @param condition 执行条件
+     * @param columns   字段列表
+     * @return children
      */
-    default Children orderByDesc(R... columns) {
+    default Children orderByDesc(boolean condition, List<R> columns) {
+        return orderBy(condition, false, columns);
+    }
+
+    default Children orderByDesc(List<R> columns) {
         return orderByDesc(true, columns);
+    }
+
+    default Children orderByDesc(R column, R... columns) {
+        return orderByDesc(true, column, columns);
     }
 
     /**
      * 排序：ORDER BY 字段, ... DESC
-     * <p>例: orderByDesc("id", "name")</p>
-     *
-     * @param condition 执行条件
-     * @param columns   字段数组
-     * @return children
      */
-    default Children orderByDesc(boolean condition, R... columns) {
-        return orderBy(condition, false, columns);
+    default Children orderByDesc(boolean condition, R column, R... columns) {
+        return orderBy(condition, false, column, columns);
     }
 
     /**
      * 排序：ORDER BY 字段, ...
-     * <p>例: orderBy(true, "id", "name")</p>
+     * <p>例: orderBy(true, "id")</p>
      *
      * @param condition 执行条件
      * @param isAsc     是否是 ASC 排序
-     * @param columns   字段数组
+     * @param column    单个字段
      * @return children
      */
-    Children orderBy(boolean condition, boolean isAsc, R... columns);
+    Children orderBy(boolean condition, boolean isAsc, R column);
+
+    /**
+     * 排序：ORDER BY 字段, ...
+     * <p>例: orderBy(true, Arrays.asList("id", "name"))</p>
+     *
+     * @param condition 执行条件
+     * @param isAsc     是否是 ASC 排序
+     * @param columns   字段列表
+     * @return children
+     */
+    Children orderBy(boolean condition, boolean isAsc, List<R> columns);
+
+    /**
+     * 排序：ORDER BY 字段, ...
+     */
+    Children orderBy(boolean condition, boolean isAsc, R column, R... columns);
 
     /**
      * ignore
