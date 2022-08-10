1331fe58ac84bf1614af506d8ce7fe83fedb08b5
fxbin <fxbin123@gmail.com>
2022-08-10 16:43:05 +0800

    :recycle: bean转化时日期数据处理

--- bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/BeanUtils.java
+++ bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/util/BeanUtils.java
@@ -75,27 +75,47 @@ public class BeanUtils extends org.springframework.beans.BeanUtils {
     public Map<String, Object> object2Map(Object object, boolean timestampDefault) {
         Assert.notNull(object, "object can't be null");
         BeanMap beanMap = BeanMap.create(object);
+        // 时间戳的处理
         if (timestampDefault) {
             Map<String, Object> map = new HashMap<>();
             beanMap.keySet().forEach(key ->
                     map.put(String.valueOf(key), DateUtils.isDateType(beanMap.get(key))? DateUtils.toEpochMilli(beanMap.get(key)) : beanMap.get(key)));
             return map;
+        } else {
+            Map<String, Object> map = new HashMap<>();
+            beanMap.keySet().forEach(key ->
+                    map.put(String.valueOf(key),
+                            DateUtils.isDateType(beanMap.get(key))? DateUtils.format(DateUtils.toLocalDateTime(DateUtils.toEpochMilli(beanMap.get(key))), DateUtils.NORM_DATETIME_PATTERN) : beanMap.get(key)));
+            return map;
         }
-        return beanMap;
     }
 
 
     /**
      * object2Map
      *
-     * @since 2020/5/8 17:01
-     * @param objectList  java.lang.Object list
+     * @since 2022/8/8 17:18
+     * @param objectList java.lang.Object list
      * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
      */
     public <T> List<Map<String, Object>> object2Map(List<T> objectList) {
+        return object2Map(objectList, false);
+    }
+
+    /**
+     * object2Map
+     *
+     * @since 2020/5/8 17:01
+     * @param objectList  java.lang.Object list
+     * @param timestampDefault 日期类型是否默认默认转时间戳
+     * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
+     */
+    public <T> List<Map<String, Object>> object2Map(List<T> objectList, boolean timestampDefault) {
         List list = new ArrayList<>();
         if(CollectionUtils.isNotEmpty(objectList)) {
-            list = objectList.stream().map(BeanMap::create).collect(Collectors.toList());
+            list = objectList.stream()
+                    .map(obj -> object2Map(obj, timestampDefault))
+                    .collect(Collectors.toList());
         }
         return list;
     }
