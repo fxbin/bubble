b9e294f29ccee21f9156f4d16222f82a51c402e3
fxbin <fxbin123@gmail.com>
2022-10-26 15:47:38 +0800

    :seedling: 增加枚举基础定义&枚举helper类

---
+++ bubble-fireworks-project/bubble-fireworks-core/src/main/java/cn/fxbin/bubble/fireworks/core/enumeration/IEnum.java
@@ -0,0 +1,27 @@
+package cn.fxbin.bubble.fireworks.core.enumeration;
+
+/**
+ * IEnum
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2022/10/26 11:48
+ */
+public interface IEnum {
+
+    /**
+     * 获取值
+     *
+     * @return int
+     */
+    int getValue();
+
+
+    /**
+     * 获取名称
+     *
+     * @return {@link String}
+     */
+    String getName();
+
+}
