e0a529e6d5fc70fb52139a11a2849c4c7335731d
fxbin <fxbin123@gmail.com>
2023-04-05 22:16:53 +0800

    :art: 优化mybatis-plus相关实现

--- bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-data-mybatis/src/main/java/cn/fxbin/bubble/fireworks/data/mybatis/autoconfigure/MybatisPlusAutoConfiguration.java
+++ bubble-fireworks-project/bubble-fireworks-starters/bubble-fireworks-starter-data-mybatis/src/main/java/cn/fxbin/bubble/fireworks/data/mybatis/autoconfigure/MybatisPlusAutoConfiguration.java
@@ -37,8 +37,8 @@ public class MybatisPlusAutoConfiguration {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 分页插件
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 分页插件, 不配置数据库类型，默认选择
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         // 乐观锁插件
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
