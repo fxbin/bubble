b87a02808512785ff38638aa751abb3242ee7055
大痴小乙 <fxbin123@gmail.com>
2023-08-20 00:28:25 +0800

    refactor: mybatis-plus BaseMapper 扩展；优化mp相关工具类实现

---
+++ bubble-spring-boot-starters/bubble-spring-boot-starter-data-mybatis-plus/src/main/java/cn/fxbin/bubble/data/mybatisplus/mapper/BaseMapperX.java
@@ -0,0 +1,199 @@
+package cn.fxbin.bubble.data.mybatisplus.mapper;
+
+import cn.fxbin.bubble.core.model.PageRequest;
+import cn.fxbin.bubble.core.model.PageResult;
+import cn.fxbin.bubble.data.mybatisplus.util.PageUtils;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.toolkit.Db;
+import org.apache.ibatis.annotations.Param;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * BaseMapperX
+ *
+ * <p>
+ *     基于 {@link BaseMapper} 扩展
+ * </p>
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2023/8/19 23:50
+ */
+public interface BaseMapperX<T> extends BaseMapper<T> {
+
+    /**
+     * 保存或更新
+     * saveOrUpdate
+     *
+     * @param entity 实体
+     * @return boolean
+     * @since 2023/8/19 23:53
+     */
+    default boolean saveOrUpdate(T entity) {
+        return Db.saveOrUpdate(entity);
+    }
+
+    /**
+     * 批量保存或更新
+     *
+     * @param collection 集合
+     */
+    default void saveOrUpdateBatch(Collection<T> collection) {
+        Db.saveOrUpdateBatch(collection);
+    }
+
+    /**
+     * 批量插入，适合大量数据插入
+     *
+     * @param entities 实体们
+     */
+    default void insertBatch(Collection<T> entities) {
+        Db.saveBatch(entities);
+    }
+
+    /**
+     * 批量插入，适合大量数据插入
+     *
+     * @param entities 实体们
+     * @param size     插入数量 Db.saveBatch 默认为 1000
+     */
+    default void insertBatch(Collection<T> entities, int size) {
+        Db.saveBatch(entities, size);
+    }
+
+
+    /**
+     * 分页查询
+     *
+     * @param pageRequest  页面请求
+     * @param queryWrapper 查询包装
+     * @return {@link PageResult}<{@link T}>
+     */
+    default PageResult<T> selectPage(PageRequest pageRequest, @Param("ew") Wrapper<T> queryWrapper) {
+        // MyBatis Plus 查询
+        IPage<T> mpPage = PageUtils.buildPage(pageRequest);
+        selectPage(mpPage, queryWrapper);
+        // 转换返回
+        return PageUtils.buildPageResult(pageRequest, mpPage);
+    }
+
+    /**
+     * 查询一条记录
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link T}
+     */
+    default T selectOne(String field, Object value) {
+        return selectOne(new QueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询一条记录
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link T}
+     */
+    default T selectOne(SFunction<T, ?> field, Object value) {
+        return selectOne(new LambdaQueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询记录数
+     *
+     * @return {@link Long}
+     */
+    default Long selectCount() {
+        return selectCount(new QueryWrapper<T>());
+    }
+
+    /**
+     * 查询记录数
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link Long}
+     */
+    default Long selectCount(String field, Object value) {
+        return selectCount(new QueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询记录数
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link Long}
+     */
+    default Long selectCount(SFunction<T, ?> field, Object value) {
+        return selectCount(new LambdaQueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询列表
+     *
+     * @return {@link List}<{@link T}>
+     */
+    default List<T> selectList() {
+        return selectList(new QueryWrapper<>());
+    }
+
+    /**
+     * 查询列表
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link List}<{@link T}>
+     */
+    default List<T> selectList(String field, Object value) {
+        return selectList(new QueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询列表
+     *
+     * @param field 字段
+     * @param value 值
+     * @return {@link List}<{@link T}>
+     */
+    default List<T> selectList(SFunction<T, ?> field, Object value) {
+        return selectList(new LambdaQueryWrapper<T>().eq(field, value));
+    }
+
+    /**
+     * 查询列表
+     *
+     * @param field  字段
+     * @param values 值
+     * @return {@link List}<{@link T}>
+     */
+    default List<T> selectList(String field, Collection<?> values) {
+        return selectList(new QueryWrapper<T>().in(field, values));
+    }
+
+    /**
+     * 查询列表
+     *
+     * @param field  字段
+     * @param values 值
+     * @return {@link List}<{@link T}>
+     */
+    default List<T> selectList(SFunction<T, ?> field, Collection<?> values) {
+        return selectList(new LambdaQueryWrapper<T>().in(field, values));
+    }
+
+}
--- bubble-spring-boot-starters/bubble-spring-boot-starter-data-mybatis-plus/src/main/java/cn/fxbin/bubble/data/mybatisplus/util/PageUtils.java
+++ bubble-spring-boot-starters/bubble-spring-boot-starter-data-mybatis-plus/src/main/java/cn/fxbin/bubble/data/mybatisplus/util/PageUtils.java
@@ -4,8 +4,10 @@ import cn.fxbin.bubble.core.model.PageRequest;
 import cn.fxbin.bubble.core.model.PageResult;
 import cn.fxbin.bubble.core.util.CollectionUtils;
 import cn.hutool.core.util.PageUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.experimental.UtilityClass;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,15 +19,16 @@ import java.util.stream.Collectors;
  * @version v1.0
  * @since 2023/4/5 21:55
  */
+@UtilityClass
 public class PageUtils extends PageUtil {
 
     /**
      * 构建{@link Page}MyBatis Plus分页对象
      *
      * @param request 请求
-     * @return {@link Page}<{@link T}>
+     * @return {@link IPage}<{@link T}>
      */
-    public <T> Page<T> buildPageObj(PageRequest request) {
+    public <T> IPage<T> buildPage(PageRequest request) {
         // 页码 + 每页数量
         Page<T> page = new Page<>(request.getPageNo(), request.getPageSize());
 
@@ -58,13 +61,13 @@ public class PageUtils extends PageUtil {
      * 构建页面结果
      *
      * @param request 请求
-     * @param page    页面
+     * @param mpPage  页面
      * @return {@link PageResult}<{@link T}>
      */
-    public <T> PageResult<T> buildPageResult(PageRequest request, Page<T> page) {
+    public <T> PageResult<T> buildPageResult(PageRequest request, IPage<T> mpPage) {
         return new PageResult<T>()
-                .compute(request.getPageSize(), request.getPageNo(), page.getTotal())
-                .setList(page.getRecords());
+                .compute(request.getPageSize(), request.getPageNo(), mpPage.getTotal())
+                .setList(mpPage.getRecords());
     }
 
 }
