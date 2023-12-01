389ff5e68e5bd6151dcf99da729ca8e253cd6fd4
fxbin <fxbin123@gmail.com>
2023-12-01 10:06:44 +0800

    refactor: :recycle: 新增 SystemUtils 工具类

---
+++ bubble-core/src/main/java/cn/fxbin/bubble/core/util/SystemUtils.java
@@ -0,0 +1,18 @@
+package cn.fxbin.bubble.core.util;
+
+import cn.hutool.system.SystemUtil;
+import lombok.experimental.UtilityClass;
+
+/**
+ * SystemUtils
+ *
+ * @author fxbin
+ * @version v1.0
+ * @since 2023/12/1 10:02
+ */
+@UtilityClass
+public class SystemUtils extends SystemUtil {
+
+
+
+}
